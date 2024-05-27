b3293f0ac27db197cfcbc662c0b196c12ff6e184
Braulinator The Destroyer <flamepekka@gmail.com>
2024-05-27 08:37:29 +0000

    Prova in classe ! yeah

---
+++ progetticpp/progetto_27.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Benvenuto su GitProoot yeah😎" << endl;
+    string name;
+    cout << "Scrivi il tuo nome: ";
+    cin >> name;
+
+    cout << "Ciao, " << name;
+    return 0;
+}
